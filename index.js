e87a8a10d1c9cbfe6ad7d905115ea891e7b17e5d
SeungminKim <ksm0057@naver.com>
2020-11-10 17:50:08 +0900

    db connect

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://Seungmin:1234@cluster0.a2i6i.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
