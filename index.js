06665c0ba3865694358946f0117d5ea8e15ee8d5
SeungminKim <ksm0057@naver.com>
2020-11-11 17:25:24 +0900

    nodemon install

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const { User } = require("./models/user");
 
 mongoose
   .connect(
@@ -10,8 +14,23 @@ mongoose
   .then(() => console.log("DB Connected"))
   .catch((err) => console.error(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.json({ hello: "hii22" });
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(5000);
