6052eac02b0da1ee7c52918f94133b2924caf8c3
SeungminKim <ksm0057@naver.com>
2020-11-13 17:22:33 +0900

    auth and logout

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cookieParser = require("cookie-parser");
 
 const config = require("./config/key");
 const { User } = require("./models/user");
+const user = require("./models/user");
+const { auth } = require("./middleware/auth");
 
 mongoose
   .connect(config.mongoURI, { useNewUrlParser: true })
@@ -16,8 +18,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.json({ hello: "hii22" });
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+  });
 });
 
 app.post("/api/users/register", (req, res) => {
@@ -26,6 +35,47 @@ app.post("/api/users/register", (req, res) => {
   user.save((err, userData) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
+      success: true,
+      userdata: userData,
+    });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  //find the email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+    }
+
+    //compare password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "Wrong Password" });
+      }
+    });
+
+    //generate Token
+    user.generateToken((err, user) => {
+      if (err) {
+        return res.statue(400).send(err);
+      }
+      res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true });
+    });
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) {
+      console.log(_id);
+      res.json({ success: false, err });
+    }
+
+    return res.status(200).send({
       success: true,
     });
   });
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -32,6 +33,7 @@ const userSchema = mongoose.Schema({
   },
 });
 
+//register
 userSchema.pre("save", function (next) {
   var user = this;
 
@@ -42,12 +44,49 @@ userSchema.pre("save", function (next) {
       bcrypt.hash(user.password, salt, function (err, hash) {
         if (err) return next(err);
         user.password = hash;
+        next();
       });
     });
   } else {
     next();
   }
 });
+
+//login
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  console.log("--" + user._id);
+  var token = jwt.sign(user._id.toHexString(), "secret");
+  user.token = token;
+  user.save((err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  jwt.verify(token, "secret", (err, decode) => {
+    user.findOne({ _id: decode, token: token }, (err, user) => {
+      if (err) {
+        cb(err);
+      }
+      cb(null, user);
+    });
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
