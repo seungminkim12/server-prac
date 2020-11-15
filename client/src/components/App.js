95a593566991d9de57202a1c37b5294ab02e4073
SeungminKim <ksm0057@naver.com>
2020-11-15 22:04:22 +0900

    about page

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import About from "./about";
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/about" component={About} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
