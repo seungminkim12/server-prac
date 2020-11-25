19312262711aa8d09a10a19b7a6343b38e0811d7
SeungminKim <ksm0057@naver.com>
2020-11-25 17:37:40 +0900

    concurrently

--- client/src/components/registerLogin/index.js
+++ client/src/components/registerLogin/index.js
@@ -1,7 +1,127 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component, useState } from "react";
+import { connect, useDispatch } from "react-redux";
 import { loginUser } from "../../actions/user_actions";
+import { useHistory } from "react-router-dom";
 
+/*
+const RegisterLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    if (event.target.name === "email") {
+      setEmail(value);
+    } else {
+      setPassword(value);
+    }
+  };
+  const displayErrors = (errors) => {
+    errors.map((error, i) => {
+      return <div key={i}>{error}</div>;
+    });
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault();
+
+    let dataToSubmit = {
+      email,
+      password,
+    };
+
+    if (isFormValid(email, password)) {
+      dispatch(loginUser(dataToSubmit)).then((response) => {
+        if (response.payload.loginSuccess) {
+          history.push("/");
+        } else {
+          setErrors(errors.concat("Failed to Login, Check your account"));
+        }
+      });
+    } else {
+      setErrors(errors.concat("Form is not valid"));
+    }
+  };
+
+  const isFormValid = ({ email, password }) => email && password;
+
+  return (
+    <div className="container">
+      <h2>Log in</h2>
+      <div className="row">
+        <form className="col 12">
+          <div className="row ">
+            <div className="input-field col s12">
+              <input
+                name="email"
+                value={email}
+                onChange={handleChange}
+                id="email"
+                type="email"
+                className="validate"
+              />
+              <label htmlFor="email">Email</label>
+              <span
+                className="helper-text"
+                data-error="Type a right type email"
+                data-success="right"
+              />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s12">
+              <input
+                name="password"
+                value={password}
+                onChange={handleChange}
+                id="password"
+                type="password"
+                className="validate"
+              />
+              <label htmlFor="password">Password</label>
+              <span
+                className="helper-text"
+                data-error="Wrong password"
+                data-success="right"
+              />
+            </div>
+          </div>
+
+          {errors && <div>{displayErrors(errors)}</div>}
+
+          <div className="row">
+            <div className="col 12">
+              <button
+                className="btn waves-effect red lighten-2"
+                type="submit"
+                name="action"
+                onClick={submitForm}
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+  };
+}
+
+export default connect(mapStateToProps)(RegisterLogin);
+
+*/
 class RegisterLogin extends Component {
   state = {
     email: "",
@@ -11,7 +131,7 @@ class RegisterLogin extends Component {
 
   displayErrors = (errors) => {
     errors.map((error, i) => {
-      <div key={i}>{error}</div>;
+      return <div key={i}>{error}</div>;
     });
   };
 
@@ -33,11 +153,6 @@ class RegisterLogin extends Component {
       this.setState({ errors: [] });
       this.props.dispatch(loginUser(dataToSubmit)).then((response) => {
         if (response.payload.loginSuccess) {
-          this.setState({
-            errors: this.state.errors.concat(
-              "Failed to Login, Check your account"
-            ),
-          });
           this.props.history.push("/");
         } else {
           this.setState({
